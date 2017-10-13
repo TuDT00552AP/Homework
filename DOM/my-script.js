d89f499e53b131848f4bc0a45eb1d30e3865def9
Đào Tuấn Tú <31761846+tuDT00552@users.noreply.github.com>
2017-10-13 09:46:15 +0700

    Update my-script.js

--- DOM/my-script.js
+++ DOM/my-script.js
@@ -1,52 +1,38 @@
-// CHECKBOY
-/*
-var checkboy = document.getElementsByClassName("checkboy");
-checkboy.onclick = function () {
-	var checkboy = this.checked;
-	var arrayboy = document.getElementsByClassName("boy");
-	// var arraygirl = document.getElementsByClassName("girl");
-	for (var i = 0; i < arrayboy.length; i++) {
-		arrayboy[i].checked = checkboy;
-	}
-}
 
-
-
-// CHECKGIRL
+// CHECK GIRL & HIDDEN BOY
 
 var checkgirl = document.getElementById("checkgirl");
 checkgirl.onclick = function () {
 	var checkgirl = this.checked;
-	var arraygirl = document.getElementsByClassName("girl");
-	var arrayboy = document.getElementsByClassName("boy");
+	var arraygirl = document.getElementsByClassName("checkgirl");
+	var hiddenboy = document.getElementsByClassName("boy");
 	for (var i = 0; i < arraygirl.length; i++) {
 		arraygirl[i].checked = checkgirl;
+		/*arraygirl[i].style.display = "inline";*/
+	}
+	if(arraygirl.checked = true){
+		for (var i = 0; i < hiddenboy.length; i++) {
+			hiddenboy[i].style.display = "none";
+		}
 	}
 }
-*/
-// HIDDEN GIRL
 
-var hiddengirl = document.getElementById("checkboy");
-hiddengirl.onclick = function() {
-	var check = this.checked;
+// CHECKBOY & HIDDENGIRL
+
+var checkboy = document.getElementById("checkboy");
+checkboy.onclick = function () {
+	var checkboy = this.checked;
+	var arrayboy = document.getElementsByClassName("checkboy");
 	var hiddengirl = document.getElementsByClassName("girl");
-	var hiddenboy = document.getElementsByClassName("boy");
-	for (var i = 0; i < hiddengirl.length; i++) {
-		hiddengirl[i].style.display = "none";
-		hiddenboy[i].style.display = "inline";	
+	for (var i = 0; i < arrayboy.length; i++) {
+		arrayboy[i].checked = checkgirl;
+		/*arrayboy[i].style.display = "inline";*/
+	}
+	if(arrayboy.checked = true){
+		for (var i = 0; i < hiddengirl.length; i++) {
+			hiddengirl[i].style.display = "none";
+		}
 	}
 }
 
-// HIDDEN BOY
-
-var hiddenboy = document.getElementById("checkgirl");
-hiddenboy.onclick = function() {
-	var hiddenboy = document.getElementsByClassName("boy");
-	var hiddengirl = document.getElementsByClassName("girl");
-	for (var i = 0; i < hiddenboy.length; i++) {
-		hiddenboy[i].style.display = "none";
-		hiddengirl[i].style.display = "inline";
-	}
-}
-
-// CHECKBOY
+/*********************************************************************/
